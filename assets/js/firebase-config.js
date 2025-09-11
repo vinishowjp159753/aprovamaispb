eb6f20eeceb70d19a5b16b36bfe80301160c4fb0
vinishowjp159753 <viniciuslobox@gmail.com>
2025-09-11 17:23:51 -0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,27 +1,21 @@
-<!-- firebase-config.js -->
-<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-auth-compat.js"></script>
-<script>
-    // Configuração do Firebase
-    const firebaseConfig = {
-        apiKey: "AIzaSyAK3U35fipoWOOIJg0yrtgmVfQ7XQxZxqY",
-        authDomain: "aprovamaispb-d4e75.firebaseapp.com",
-        projectId: "aprovamaispb-d4e75",
-        storageBucket: "aprovamaispb-d4e75.firebasestorage.app",
-        messagingSenderId: "828577018900",
-        appId: "1:828577018900:web:473d8c3fc2685d3f192301"
-    };
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
-    // Inicializar Firebase
-    if (typeof firebase !== 'undefined' && !firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig);
-    }
-    
-    // Referência para Firestore
-    const db = firebase.firestore();
-    
-    // Exportar para uso global
-    window.db = db;
-    window.firebaseApp = firebase;
-</script>
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAK3U35fipoWOOIJg0yrtgmVfQ7XQxZxqY",
+  authDomain: "aprovamaispb-d4e75.firebaseapp.com",
+  projectId: "aprovamaispb-d4e75",
+  storageBucket: "aprovamaispb-d4e75.firebasestorage.app",
+  messagingSenderId: "828577018900",
+  appId: "1:828577018900:web:473d8c3fc2685d3f192301"
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
