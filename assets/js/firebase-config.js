69a601b436b0eb47ec33099cccc177597a220f2c
vinishowjp159753 <viniciuslobox@gmail.com>
2025-09-12 14:58:59 -0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,18 +1,19 @@
 // assets/js/firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
+// Importações do Firebase
+import { initializeApp as firebaseInitializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+
+// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAK3U35fipoWOOIJg0yrtgmVfQ7XQxZxqY",
   authDomain: "aprovamaispb-d4e75.firebaseapp.com",
   projectId: "aprovamaispb-d4e75",
-  storageBucket: "aprovamaispb-d4e75.appspot.com",
+  storageBucket: "aprovamaispb-d4e75.firebasestorage.app",
   messagingSenderId: "828577018900",
   appId: "1:828577018900:web:473d8c3fc2685d3f192301"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Exporta para outros módulos
-export { app, db };
+// Função que inicializa o Firebase e retorna a app
+export function initializeApp() {
+  return firebaseInitializeApp(firebaseConfig);
+}
