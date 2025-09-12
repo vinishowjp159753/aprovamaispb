00cb6ec3724d95b5a521ee24f3f80632e11d4021
vinishowjp159753 <viniciuslobox@gmail.com>
2025-09-12 16:26:19 -0300

    Add files via upload

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,6 +1,9 @@
-import { initializeApp as firebaseInitializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+// assets/js/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
+// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAK3U35fipoWOOIJg0yrtgmVfQ7XQxZxqY",
   authDomain: "aprovamaispb-d4e75.firebaseapp.com",
@@ -10,5 +13,9 @@ const firebaseConfig = {
   appId: "1:828577018900:web:473d8c3fc2685d3f192301"
 };
 
-const app = firebaseInitializeApp(firebaseConfig);
-export const db = getFirestore(app);
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
