9d8d5bb7f1d3b93fa56e274c127b0bbffc8d528b
vinishowjp159753 <viniciuslobox@gmail.com>
2025-09-15 12:05:10 -0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,19 +1,21 @@
 // assets/js/firebase-config.js
-
-// Importações do Firebase
-import { initializeApp as firebaseInitializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-
-// Configuração do Firebase
+// Firebase v8 - compatível com db.collection
 const firebaseConfig = {
   apiKey: "AIzaSyAK3U35fipoWOOIJg0yrtgmVfQ7XQxZxqY",
   authDomain: "aprovamaispb-d4e75.firebaseapp.com",
   projectId: "aprovamaispb-d4e75",
-  storageBucket: "aprovamaispb-d4e75.firebasestorage.app",
+  storageBucket: "aprovamaispb-d4e75.appspot.com",
   messagingSenderId: "828577018900",
   appId: "1:828577018900:web:473d8c3fc2685d3f192301"
 };
 
-// Função que inicializa o Firebase e retorna a app
-export function initializeApp() {
-  return firebaseInitializeApp(firebaseConfig);
-}
+// Inicializa Firebase com namespace antigo
+firebase.initializeApp(firebaseConfig);
+
+// Firestore e Auth disponíveis globalmente
+const db   = firebase.firestore();
+const auth = firebase.auth();
+
+// Torna acessível no window (se precisar em outros scripts)
+window.db   = db;
+window.auth = auth;
