b78a785e2f136a2d22bb8e256528c956c1d4cdda
vinishowjp159753 <viniciuslobox@gmail.com>
2025-09-15 12:08:38 -0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,21 +1,19 @@
 // assets/js/firebase-config.js
-// Firebase v8 - compatível com db.collection
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAK3U35fipoWOOIJg0yrtgmVfQ7XQxZxqY",
   authDomain: "aprovamaispb-d4e75.firebaseapp.com",
   projectId: "aprovamaispb-d4e75",
-  storageBucket: "aprovamaispb-d4e75.appspot.com",
+  storageBucket: "aprovamaispb-d4e75.firebasestorage.app",
   messagingSenderId: "828577018900",
   appId: "1:828577018900:web:473d8c3fc2685d3f192301"
 };
 
-// Inicializa Firebase com namespace antigo
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db  = getFirestore(app);
+const auth = getAuth(app);
 
-// Firestore e Auth disponíveis globalmente
-const db   = firebase.firestore();
-const auth = firebase.auth();
-
-// Torna acessível no window (se precisar em outros scripts)
-window.db   = db;
-window.auth = auth;
+export { app, db, auth };
