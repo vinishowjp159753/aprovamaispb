ff8e98b3669b59771cadf1ddc348fe5b742dd141
vinishowjp159753 <viniciuslobox@gmail.com>
2025-09-25 16:08:59 -0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAK3U35fipoWOOIJg0yrtgmVfQ7XQxZxqY",
-  authDomain: "aprovamaispb-d4e75.firebaseapp.com",
-  projectId: "aprovamaispb-d4e75",
-  storageBucket: "aprovamaispb-d4e75.firebasestorage.app",
-  messagingSenderId: "828577018900",
-  appId: "1:828577018900:web:473d8c3fc2685d3f192301"
+  apiKey: "AIzaSyDYT4OkPkJHkXE44jqIcaWtXL-f105JNoE",
+  authDomain: "aprovamaispb-6ea02.firebaseapp.com",
+  projectId: "aprovamaispb-6ea02",
+  storageBucket: "aprovamaispb-6ea02.firebasestorage.app",
+  messagingSenderId: "980253365163",
+  appId: "1:980253365163:web:e1d116769cd8031dd1b8ee"
 };
 
 const app = initializeApp(firebaseConfig);
