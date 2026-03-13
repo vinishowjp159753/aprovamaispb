ac9c8eb3ab2a07d1e6fbb657d2cc4bc3a3691cf5
vinishowjp159753 <viniciuslobox@gmail.com>
2026-03-13 16:51:55 -0300

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,20 +1,24 @@
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js"
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js"
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js"
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js"
 
 const firebaseConfig = {
-apiKey:"AIzaSyA8IOr3_gzyCbYqk8RH6lwwgtQLQ0u5ch0",
-authDomain:"aprovamaispb-73d39.firebaseapp.com",
-projectId:"aprovamaispb-73d39",
-storageBucket:"aprovamaispb-73d39.firebasestorage.app",
-messagingSenderId:"1052452508337",
-appId:"1:1052452508337:web:0b7919f981aa487c410890"
-};
 
-const app = initializeApp(firebaseConfig);
+apiKey: "AIzaSyBRm4yix2fC2PhAVjTtbWlR-azz0bAglpo",
+authDomain: "aprovamaispb2-3bc78.firebaseapp.com",
+projectId: "aprovamaispb2-3bc78",
+storageBucket: "aprovamaispb2-3bc78.firebasestorage.app",
+messagingSenderId: "830264910104",
+appId: "1:830264910104:web:0c0c6a9b1930c155c47e6d",
+measurementId: "G-PM06K116GT"
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+}
+
+const app = initializeApp(firebaseConfig)
+
+export const analytics = getAnalytics(app)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
