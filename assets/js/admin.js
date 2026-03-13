1326f196961c05604d7b2b4ccd8a18e6832e7513
vinishowjp159753 <viniciuslobox@gmail.com>
2026-03-13 16:54:20 -0300

    Refactor uploadMaterial function and event listener

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -9,26 +9,39 @@ await signOut(auth)
 window.location.href="login.html"
 })
 
-document.getElementById("btnUpload").addEventListener("click",async ()=>{
+document.getElementById("btnUpload").addEventListener("click", uploadMaterial)
 
-let titulo=document.getElementById("titulo").value
-let materia=document.getElementById("materia").value
-let arquivo=document.getElementById("arquivo").files[0]
+async function uploadMaterial(){
 
-if(!arquivo){alert("Selecione um arquivo");return}
+let titulo = document.getElementById("titulo").value
+let materia = document.getElementById("materia").value
+let professor = document.getElementById("professor").value
+let curso = document.getElementById("curso").value
 
-let storageRef=ref(storage,"materiais/"+arquivo.name)
+let arquivo = document.getElementById("arquivo").files[0]
+
+if(!arquivo){
+alert("Selecione um arquivo")
+return
+}
+
+let storageRef = ref(storage,"materiais/"+Date.now()+"_"+arquivo.name)
 
 await uploadBytes(storageRef,arquivo)
 
-let url=await getDownloadURL(storageRef)
+let url = await getDownloadURL(storageRef)
 
 await addDoc(collection(db,"materiais"),{
+
 titulo:titulo,
 materia:materia,
-url:url
+professor:professor,
+curso:curso,
+url:url,
+dataUpload:new Date()
+
 })
 
-document.getElementById("statusUpload").innerText="Upload concluído!"
+alert("Material enviado com sucesso")
 
-})
+}
