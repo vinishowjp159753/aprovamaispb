993a0275a22d696a727a4c8c6cd62921535a8ee0
vinishowjp159753 <viniciuslobox@gmail.com>
2026-03-15 14:43:27 -0300

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -5,13 +5,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js"
 
 const firebaseConfig = {
-apiKey:"YOUR_KEY",
-authDomain:"YOUR_DOMAIN",
-projectId:"YOUR_PROJECT",
-storageBucket:"YOUR_BUCKET",
-messagingSenderId:"ID",
-appId:"APPID"
-}
+  apiKey: "AIzaSyBRm4yix2fC2PhAVjTtbWlR-azz0bAglpo",
+  authDomain: "aprovamaispb2-3bc78.firebaseapp.com",
+  projectId: "aprovamaispb2-3bc78",
+  storageBucket: "aprovamaispb2-3bc78.firebasestorage.app",
+  messagingSenderId: "830264910104",
+  appId: "1:830264910104:web:0c0c6a9b1930c155c47e6d",
+  measurementId: "G-PM06K116GT"
+};
 
 const app=initializeApp(firebaseConfig)
 
